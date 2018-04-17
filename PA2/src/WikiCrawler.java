d0f14a8f269cfd468b18a1207eb9b11d77179a5e
RyanRadomski7 <radomski@iastate.edu>
2018-04-16 21:59:01 -0500

    added visited

--- PA2/src/WikiCrawler.java
+++ PA2/src/WikiCrawler.java
@@ -18,6 +18,7 @@ public class WikiCrawler {
 	public String seedUrl;
 	public int max;
 	public ArrayList<String> topics;
+	public HashSet<String> visited;
 	public String fileName;
 	
 	public WikiCrawler(String seedUrl, int max, ArrayList<String> topics, String fileName) {
